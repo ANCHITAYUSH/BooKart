42c85757b31780da6cb48221f29c175af1c8b53b
Anchit Ayush Guria <Anchit.Guria@technoforte.co.in>
2024-05-28 17:56:53 +0530

    Create layout

---
+++ src/components/BookList.jsx
@@ -0,0 +1,30 @@
+import { React, useState, useEffect} from 'react';
+import '../App'
+import { API_URL } from '../API';
+import axios from 'axios';
+
+const BookList = () => {
+
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    axios.get(API_URL).then((res) => {
+      console.log(res.data);
+      setBooks(res.data);
+    }).catch((err) => console.error(err));
+  }, []);
+
+  return (
+    <div className='book-list'>
+      {books.map((book) => {
+        <div key={book.id}>
+          <div><h2>{book.title}</h2></div>
+          <div><img src={book.image_url} alt='#'/></div>
+          <div><button>Add to Favorites</button></div>
+        </div>
+      })}
+    </div>
+  );
+}
+
+export default BookList;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <h2>BooKarT</h2>
+      <span>Your Favourites</span>
+    </div>
+  );
+}
+
+export default Navbar;
